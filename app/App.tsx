710abc7fdee1a4b016dac62fb8d21a6bc555562f
Sage <zhang@saget.cn>
2020-12-31 23:50:31 +0800

    add slate ts react .

---
+++ app/App.tsx
@@ -0,0 +1,15 @@
+import React, { Component} from "react";
+// import "./App.css";
+import Slate from "./components/Slate";
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <Slate />
+      </div>
+    );
+  }
+}
+
+export default App;
